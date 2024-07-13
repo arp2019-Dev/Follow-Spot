f218932181eac6051367ac39a678fb5fc7b49213
arp2019-Dev <akhilrobotics@icloud.com>
2024-07-13 15:19:03 -0700

    version 1 code

---
+++ src/main.cpp
@@ -0,0 +1,72 @@
+#include <Arduino.h>
+
+#include <AccelStepper.h>
+
+// Pinouts
+const int stepPinX = 2;
+const int dirPinX = 3;
+const int stepPinY = 6;
+const int dirPinY = 7;
+const int lightPin = 8;
+const int lightTrigger = 9;
+bool lightState = false;
+
+// Motor Initialization
+AccelStepper stepperX(1, stepPinX, dirPinX);
+AccelStepper stepperY(1, stepPinY, dirPinY);
+
+
+long desiredPositionX = 0;
+long desiredPositionY = 0;
+
+void setup() {
+  Serial.begin(9600);
+  
+  // Motor X setup
+  stepperX.setMaxSpeed(1000); 
+  stepperX.setAcceleration(500); 
+
+  // Motor Y setup
+  stepperY.setMaxSpeed(1000); 
+  stepperY.setAcceleration(500);
+  
+  pinMode(lightTrigger, INPUT_PULLUP);
+  pinMode(lightPin, OUTPUT);
+  
+  lightState = false; 
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String command = Serial.readStringUntil('\n');
+    int commaIndex = command.indexOf(',');
+    if (commaIndex > 0) {
+      int stepsX = command.substring(0, commaIndex).toInt();
+      int stepsY = command.substring(commaIndex + 1).toInt();
+
+    }
+  }
+  
+
+  stepperX.moveTo(desiredPositionX);
+  stepperY.moveTo(desiredPositionY);
+
+  stepperX.run();
+  stepperY.run();
+  
+
+  int buttonState = digitalRead(lightTrigger);
+  if (buttonState == LOW) {
+    delay(50);
+    if (lightState == false) {
+      lightState = true;
+    } else {
+      lightState = false;
+    }
+
+    while (digitalRead(lightTrigger) == LOW) {
+      delay(10);
+    }
+  }
+
+}
